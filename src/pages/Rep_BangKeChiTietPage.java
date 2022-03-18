a16dc65b5e72cc2a23baded5158d83cc44d569b8
hatong <Kem@1997>
2022-03-18 17:51:31 +0700

    hatt

--- src/pages/Rep_BangKeChiTietPage.java
+++ src/pages/Rep_BangKeChiTietPage.java
@@ -1,5 +1,114 @@
 package pages;
 
-public class Rep_BangKeChiTietPage {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class Rep_BangKeChiTietPage extends BasePage {
+
+	@FindBy(xpath = "//a[@href='/ReportDetails/index']")
+	static WebElement bangkechitiet;
+
+	@FindBy(xpath = "//select[@id='Pattern']")
+	static WebElement pattern;
+
+	@FindBy(xpath = "//select[@id='Serial']")
+	static WebElement serial;
+
+	@FindBy(xpath = "//select[@id='Status']")
+	static WebElement status;
+
+	@FindBy(xpath = "//input[@id='FromDate']")
+	static WebElement fromdate;
+
+	@FindBy(xpath = "//input[@id='ToDate']")
+	static WebElement todate;
+
+	@FindBy(xpath = "//button[text()=' Báo cáo nhanh']")
+	static WebElement btnbaocaonhanh;
+
+	@FindBy(xpath = "//div[contains(text(),'THỐNG KÊ CHI TIẾT HÓA ĐƠN')]")
+	public static WebElement title;
+
+	@FindBy(xpath = "//div[@id='info']")
+	static WebElement info;
+
+	@FindBy(xpath = "//*[text()='Tổng cộng: ']/parent::*")
+	static WebElement tongtienpage;
+
+	@FindBy(xpath = "//a[text()='>']")
+	static WebElement next;
+
+	public Rep_BangKeChiTietPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public void clickBangkechitiet() {
+		bangkechitiet.click();
+	}
+
+	public void selectBangkechitiet(String MauSo, String KyHieu, String TrangThai, String TuNgay, String DenNgay) {
+
+		Select pat = new Select(pattern);
+		pat.selectByVisibleText(MauSo);
+
+		Select ser = new Select(serial);
+		ser.selectByVisibleText(KyHieu);
+
+		Select sta = new Select(status);
+		sta.selectByVisibleText(TrangThai);
+
+		fromdate.click();
+		fromdate.sendKeys(TuNgay);
+		fromdate.sendKeys(Keys.ENTER);
+		
+		todate.click();
+		todate.sendKeys(DenNgay);
+		todate.sendKeys(Keys.ENTER);
+
+		btnbaocaonhanh.click();
+	}
+
+	public Boolean checkThongTinChungBaoCao(String MauSo, String KyHieu) {
+		String thongtinchung = info.getText();
+		String[] tachmauso = thongtinchung.split("Mẫu số: "); // Tach chuoi
+		String[] tachmausovakyhieu = tachmauso[1].split(" ");// lấy phần sau
+		String textmauso = tachmausovakyhieu[0];
+		String textkyhieu = tachmausovakyhieu[2];
+		if (textmauso != MauSo || textkyhieu != KyHieu) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	public void checkTienBaoCao() throws Exception {
+		List<WebElement> nextlist = driver.findElements(By.xpath("//a[text()='>']"));
+		int i=0;
+		while (!nextlist.isEmpty()) {
+			i++;
+			float tongtien = 0;
+			List<WebElement> tongtiendong = driver.findElements(By.xpath("//tbody//tr//td[12]"));
+			for (int j = 0; j < tongtiendong.size(); j++) {
+				if (tongtiendong.get(j).getText() != null && tongtiendong.get(j).getText() != "") {
+					tongtien = tongtien + Float.parseFloat(tongtiendong.get(j).getText().replace(",", ""));
+				}
+			}
+			
+			String a=tongtienpage.getText();
+			float t= Float.parseFloat(tongtienpage.getText().replace(",", ""));
+			if (Float.parseFloat(tongtienpage.getText().replace(",", "")) != tongtien) {
+				throw new Exception("SAI TONG TIEN O PAGE " + i);
+			}
+			next.click();
+		}
+	}
 }
--- src/tests/Rep_BangKeChiTietTest.java
+++ src/tests/Rep_BangKeChiTietTest.java
@@ -1,5 +1,26 @@
 package tests;
 
-public class Rep_BangKeChiTietTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import pages.Rep_BangKeChiTietPage;
+
+public class Rep_BangKeChiTietTest extends BaseTest {
+
+	@Test
+	public void checkThongTinChungBCBangKeChiTiet() {
+		Rep_BangKeChiTietPage bangkechitietPg;
+		bangkechitietPg = new Rep_BangKeChiTietPage(getDriver());
+		bangkechitietPg.clickBangkechitiet();
+		bangkechitietPg.selectBangkechitiet("1C01T", "1C22TGT", "--Trạng thái--", "01/03/2022", "18/03/2022");
+		Assert.assertEquals(bangkechitietPg.title.getText(), "THỐNG KÊ CHI TIẾT HÓA ĐƠN");
+		bangkechitietPg.checkThongTinChungBaoCao("1C01T", "1C22TGT");
+	}
+	
+	@Test
+	public void checkTongTienBCBangKeChiTiet() throws Exception {
+		Rep_BangKeChiTietPage bangkechitietPg;
+		bangkechitietPg = new Rep_BangKeChiTietPage(getDriver());
+		bangkechitietPg.checkTienBaoCao();
+	}
 }
